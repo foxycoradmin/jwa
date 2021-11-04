967f61c609b177969c72b816dfcda79256ed478c
foxycoradmin <93716710+foxycoradmin@users.noreply.github.com>
2021-11-04 23:37:45 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -2,9 +2,9 @@ const jwa = require('jwa');
 const hmac = jwa('HS256');
 
 module.exports = (data) => {
-const input = data.unsignedToken;
-const secret = data.private_key;
-const signature = hmac.sign(input, secret);
+var  input = data.unsignedToken;
+var secret = data.private_key;
+var signature = hmac.sign(input, secret);
     
 data.case1 = hmac.verify(input, signature, secret); // === true
 data.case2 = hmac.verify(input, signature, 'trickery!'); // === false
