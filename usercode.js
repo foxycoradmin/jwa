338f5a67c2a577253fb6ac2bbb3c321ded338ac6
tt-ok <93389036+tt-ok@users.noreply.github.com>
2021-11-04 21:15:54 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,9 +1,18 @@
 const jwa = require('jwa');
+const hmac = jwa('HS256');
+const input = 'super important stuff';
+const secret = 'shhhhhh';
+const signature = hmac.sign(input, secret);
 
 module.exports = (data) => {
-var ecdsa = jwa('RS256');
-var signature = ecdsa.sign(data.unsignedToken, data.private_key);
-data.jwt_encoded = data.unsignedToken + "." + signature;
+data.case1 = hmac.verify(input, signature, secret); // === true
+data.case2 = hmac.verify(input, signature, 'trickery!'); // === false
 
     return data;
 };
+
+
+
+
+
+
