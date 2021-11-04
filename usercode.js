4a7873c07b4d9184ba48e91abc8bf23e80ef2264
tt-ok <93389036+tt-ok@users.noreply.github.com>
2021-11-04 20:52:14 +0200

    Create usercode.js

---
+++ usercode.js
@@ -0,0 +1,9 @@
+const jwa = require('node-jwa');
+
+module.exports = (data) => {
+var ecdsa = jwa('RS256');
+var signature = ecdsa.sign(data.unsignedToken, data.private_key);
+data.jwt_encoded = data.unsignedToken + "." + signature;
+
+    return data;
+};
